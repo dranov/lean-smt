69860d889d74e90820fdb632457cc15bc8924e11
George Pîrlea <george@pirlea.net>
2025-06-19 11:34:54 +0800

    Bump to v4.20.0

--- lakefile.lean
+++ lakefile.lean
@@ -3,13 +3,13 @@ import Lake
 open Lake DSL
 
 require auto from
-  git "https://github.com/leanprover-community/lean-auto.git" @ "cd38a184972438d191f1be853e60caadebfc12ec"
+  git "https://github.com/leanprover-community/lean-auto.git" @ "ec89836a79a3266ebd840fb0df59ca4c972a4c25"
 
 require cvc5 from
-  git "https://github.com/abdoo8080/lean-cvc5.git" @ "fc92260"
+  git "https://github.com/dranov/lean-cvc5.git" @ "v4.20.0"
 
 require mathlib from
-  git "https://github.com/leanprover-community/mathlib4.git" @ "v4.20.0-rc5"
+  git "https://github.com/leanprover-community/mathlib4.git" @ "v4.20.0"
 
 package smt
 
